58870d1444ea875016d9b100f64153191c80b0b1
Sami Badawi <sami.badawi@gmail.com>
2019-02-14 21:11:23 -0500

    More complete Google Vision API results

--- src/api_respons_models.ts
+++ src/api_respons_models.ts
@@ -1,6 +1,10 @@
 /**
  * Interfaces used in Google Vision API results
  */
+export interface Point2D {
+    x: number;
+    y: number;
+}
 
 export interface Point3D {
     x: number;
@@ -8,13 +12,32 @@ export interface Point3D {
     z: number;
 }
 
-export interface FaceFeature          {
+export interface FaceFeature {
     type: string;
     position: Point3D;
 }
 
+export interface Vertex2Array {
+    vertices: Array<Point2D>;
+    normalizedVertices?: any;
+}
+
 export interface FaceAnnotation {
     landmark: Array<FaceFeature>;
+    boundingPoly: Array<any>;
+    fdBoundingPoly: Array<any>;
+    rollAngle: number;
+    panAngle: number;
+    tiltAngle: number;
+    detectionConfidence: number;
+    landmarkingConfidence: number;
+    joyLikelihood: string;
+    sorrowLikelihood: string;
+    angerLikelihood: string;
+    surpriseLikelihood: string;
+    underExposedLikelihood: string;
+    blurredLikelihood: string;
+    headwearLikelihood: string;
 }
 
 export interface ApiVisionResponse {
